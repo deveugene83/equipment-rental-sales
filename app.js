22137db24006f42c4036f5c28dc6e35c0af6c11d
Eugene Cabauatan <e.cabauatan@archirodon.net>
2018-09-30 09:49:17 +0300

    worked with public and views

--- app.js
+++ app.js
@@ -2,23 +2,27 @@ var express       = require("express"),
     app           = express();
 
 
-// app.set("view engine", "ejs");
-// app.use(express.static(__dirname + "/public"));
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
 
 
 //Routes
 app.get("/", function(req, res){
-   res.send("Hi There!");
+   res.render("index");
 });
 
-app.get("/hello", function(req, res){
-   res.send("hello There!");
+app.get("/landing", function(req, res){
+   res.render("landing");
 });
 
-app.listen(process.env.PORT, process.env.IP, function(){
-   console.log("Server has started!"); 
+app.get("/dog", function(req, res){
+   res.send("Dog says Hi!");
 });
 
-// app.listen(3000, function(){
+// app.listen(process.env.PORT, process.env.IP, function(){
 //    console.log("Server has started!"); 
 // });
+
+app.listen(3000, function(){
+   console.log("Server has started!"); 
+});
